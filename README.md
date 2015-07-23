c6980c54e93085d6c8d17fe0ed2af3fa297317fd
zamani2015 <zamani@tmsk.uitm.edu.my>
2015-07-23 15:31:09 +0800

    test

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# Module3_Assignment
-Module3_Assignment
+1. The purpose of this assignment is to collect, work with, and clean a data set. The end of this R script will provide us with a tidy data named as "finaloutcome_tidydata.txt"
+
+2. A full description of this assignment is available at the site: 
+
+http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+
+3. The data for the project at:
+
+https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+
+4. One R script called run_analysis.R contains the following: 
+
+(i)   Merges the training and the test sets to create one data set.
+(ii)  Extracts only the measurements on the mean and standard deviation for each measurement. 
+(iii) Uses descriptive activity names to name the activities in the data set
+(iv)  Appropriately labels the data set with descriptive variable names. 
+(v)   From the data set in step (iv), creates a second, independent tidy data set with the average of each variable for each activity and each subject.
--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,6 @@
 # The purpose of this R script is to perform:
 
-# Instruction 1. Merges the training and the test sets to create one data set.
+## Step (i) - Merges the training and the test sets to create one data set.
 
 tmp1 <- read.table("train/X_train.txt")
 tmp2 <- read.table("test/X_test.txt")
@@ -14,7 +14,7 @@ tmp1 <- read.table("train/y_train.txt")
 tmp2 <- read.table("test/y_test.txt")
 Y <- rbind(tmp1, tmp2)
 
-# 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+## Step (ii) - Extracts only the measurements on the mean (-mean) and standard deviation (-std) for each measurement.
 
 features <- read.table("features.txt")
 indices_of_good_features <- grep("-mean\\(\\)|-std\\(\\)", features[, 2])
@@ -23,20 +23,21 @@ names(X) <- features[indices_of_good_features, 2]
 names(X) <- gsub("\\(|\\)", "", names(X))
 names(X) <- tolower(names(X))
 
-# 3. Uses descriptive activity names to name the activities in the data set.
+## Step (iii) - Uses descriptive activity names to name the activities in the data set
 
 activities <- read.table("activity_labels.txt")
 activities[, 2] = gsub("_", "", tolower(as.character(activities[, 2])))
 Y[,1] = activities[Y[,1], 2]
 names(Y) <- "activity"
 
-# 4. Appropriately labels the data set with descriptive activity names.
+## Step (iv) - Appropriately labels the data set with descriptive variable names. 
 
 names(S) <- "subject"
 cleaned <- cbind(S, Y, X)
 write.table(cleaned, "merged_clean_data.txt")
+dim(cleaned)
 
-# 5. Creates a 2nd, independent tidy data set with the average of each variable for each activity and each subject.
+## Step (v) - From the data set in step (iv), creates a second, independent tidy data set with the average of each variable for each activity and each subject
 
 uniqueSubjects = unique(S)[,1]
 numSubjects = length(unique(S)[,1])
@@ -55,3 +56,4 @@ for (s in 1:numSubjects) {
   }
 }
 write.table(result, "finaloutcome_tidydata.txt")
+dim(result)
